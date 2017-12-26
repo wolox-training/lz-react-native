a39c92e64bda20c9900181618cca9ab1d831cccf
LucasZibell <lucas.zibell@wolox.com.ar>
2017-12-26 11:35:31 -0300

    refactor ErrorMsg

--- React/my-app/src/components/ErrorMsg/index.js
+++ React/my-app/src/components/ErrorMsg/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import stylesCss from "./styles.css";
 
-const ErrorMsg = ({ message }) => {
-  if (message) {
-    return <h1 className="error"> {message} </h1>;
-  }
-  return <h1> </h1>;
-};
+const ErrorMsg = ({ message }) =>
+  message && <h1 className="error"> {message} </h1>;
+
 export default ErrorMsg;
