e0e0610eeda7919d6a3f219b9b8bd6716fc25e9a
LucasZibell <lucas.zibell@wolox.com.ar>
2018-10-16 12:16:25 -0300

    book list

--- trainingRN/src/screens/bookList/index.js
+++ trainingRN/src/screens/bookList/index.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
-import { Text, View } from 'react-native';
+import { Text, ScrollView, Image, View } from 'react-native';
 
+import books from '../../constants/books';
+import styles from './styles';
+
+import defaultCover from '../../assets/default_cover.jpg'
 import { BOOK_LIST} from '../../constants/constant';
 
 class BookList extends Component {
@@ -9,7 +13,17 @@ class BookList extends Component {
   };
   render() {
     return (
-    <View />
+    <ScrollView style={styles.container}>
+      {books.map(({ id, image_url, title, author }) =>
+        <View style={styles.book} key={id}>
+          <Image style={styles.image} source={ image_url ? { uri: image_url } : defaultCover }/>
+          <View style={styles.text}>
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.author}>{author}</Text>
+          </View>
+        </View>
+        )}
+    </ScrollView>
     )
   }
 }
---
+++ trainingRN/src/screens/bookList/styles.js
@@ -0,0 +1,32 @@
+import { StyleSheet} from 'react-native';
+import colors from '../../constants/colors'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.aliceBlue
+  },
+  book: {
+    flexDirection: 'row',
+    marginBottom: 10
+  },
+  image: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    marginRight:10
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  author: {
+    fontSize: 14
+  },
+  text: {
+    flexDirection: 'column',
+    marginBottom: 5
+  }
+});
+
+export default styles;
